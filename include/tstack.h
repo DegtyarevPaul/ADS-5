7ecb6e220a09fcf6deb1fc42e1ef3a573f3994bc
DegtyarevPaul <99130466+DegtyarevPaul@users.noreply.github.com>
2022-04-03 18:40:20 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,9 +1,33 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2022 DegtyarevPaul
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
+const int size = 20;
+
 template<typename T, int size>
 class TStack {
+private:
+  T arr[20];
+  int top;
 
+ public:
+  TStack() : top(-1) { }
+  T get() const {
+  return arr[top];
+  }
+  bool isEmpty() const {
+  return top == -1;
+  }
+  bool isFull() const {
+  return top == size - 1;
+  }
+  void pop() {
+  if (top >= 0)
+    top--;
+  }
+  void push(T item) {
+  if (top < size - 1)
+    arr[++top] = item;
+  }
 };
 #endif  // INCLUDE_TSTACK_H_
