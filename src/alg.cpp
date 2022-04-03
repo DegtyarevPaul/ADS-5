34142f16c56ab63efed447c0b6b7d35224c55f9c
DegtyarevPaul <99130466+DegtyarevPaul@users.noreply.github.com>
2022-04-03 23:08:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,19 +4,80 @@
 #include "tstack.h"
 
 std::string infx2pstfx(std::string inf) {
-  TStack<char, size> csStack;
-  std::string str;
-  std::map<char, int> ;
-  ['('] = 0;
-  [')'] = 0;
-  ['+'] = 1;
-  ['-'] = 1;
-  ['*'] = 2;
-  ['/'] = 2;
-  
-}
+  TStack<char, 20> mpStack;
+    std::string str;
+    std::map<char, int> mp;
+    mp['('] = 0;
+    mp[')'] = 0;
+    mp['+'] = 1;
+    mp['-'] = 1;
+    mp['*'] = 2;
+    mp['/'] = 2;
+    for (char ch : inf) {
+        if (mp.find(ch) == mp.end()) {
+            str += ch;
+            str += ' ';
+        } else {
+            if (ch == ')') {
+                while (mpStack.get() != '(') {
+                    str += mpStack.get();
+                    str += ' ';
+                    mpStack.pop();
+                }
+                mpStack.pop();
+            } else if (ch == '(' || mpStack.isEmpty()) {
+                mpStack.push(ch);
+            } else if (mp[ch] > mp[mpStack.get()]) {
+                mpStack.push(ch);
+            } else {
+                while (!mpStack.isEmpty() && mp[mpStack.get()] >= mp[ch]) {
+                    str += mpStack.get();
+                    str += ' ';
+                    mpStack.pop();
+                }
+                mpStack.push(ch);
+            }
+        }
+    }
+    while (!mpStack.isEmpty()) {
+        str += mpStack.get();
+        str += ' ';
+        mpStack.pop();
+    }
+    str.pop_back();
+    return str;
+  }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  TStack<int, 20> answStack;
+  std::string mp = "+-*/";
+  for (char ch : str) {
+    if (ch == ' ') continue;
+    std::size_t op = mp.find(ch);
+    if (op == std::string::npos) {
+      answStack.push(ch & 0xF);
+    } else {
+      int arg2 = answStack.get();
+      answStack.pop();
+      int arg1 = answStack.get();
+      answStack.pop();
+      int result;
+      switch (mp[op]) {
+      case '+':
+        result = arg1 + arg2;
+        break;
+      case '-':
+        result = arg1 - arg2;
+        break;
+      case '*':
+        result = arg1 * arg2;
+        break;
+      case '/':
+        result = arg1 / arg2;
+        break;
+      }
+            answStack.push(result);
+        }
+    }
+    return answStack.get();  return 0;
 }
