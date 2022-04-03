dbd0f465a51b32521f8ae8985b8383a9b1b9dbe7
DegtyarevPaul <99130466+DegtyarevPaul@users.noreply.github.com>
2022-04-03 20:41:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,19 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2022 DegtyarevPaul 
 #include <string>
 #include <map>
 #include "tstack.h"
 
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+  TStack<char, size> csStack;
+  std::string str;
+  std::map<char, int> ;
+  ['('] = 0;
+  [')'] = 0;
+  ['+'] = 1;
+  ['-'] = 1;
+  ['*'] = 2;
+  ['/'] = 2;
+  
 }
 
 int eval(std::string pref) {
